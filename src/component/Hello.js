353aa79adcd5d73df7d20e8a07b2166fb5f67d24
graceJoo <gracejoo26@gmail.com>
2024-05-15 17:02:54 +0900

    6강- 이벤트처리리

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,12 +1,31 @@
-import World from "./World";
-import styles from "./Hello.module.css";
 export default function Hello() {
+  function showName() {
+    console.log("Mike");
+  }
+  function showAge(age) {
+    console.log(age);
+  }
+  function showText(txt) {
+    console.log(txt);
+  }
   return (
     <div>
-      <p>
-        Hello <World />
-        <div className={styles.box}>app</div>
-      </p>
+      <h1>Hello</h1>
+      <button onClick={showName}>show Name</button>
+      <button
+        onClick={() => {
+          showAge(30);
+        }}
+      >
+        show Age
+      </button>
+      <input
+        type="text"
+        onChange={(e) => {
+          const txt = e.target.value;
+          console.log(txt);
+        }}
+      ></input>
     </div>
   );
 }
