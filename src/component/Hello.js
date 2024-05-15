0783104173a5223efc20fe53d945305d1515b3d9
graceJoo <gracejoo26@gmail.com>
2024-05-15 16:48:59 +0900

    5강- 리액트 스타일 상속

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,9 +1,11 @@
 import World from "./World";
+import styles from "./Hello.module.css";
 export default function Hello() {
   return (
     <div>
       <p>
         Hello <World />
+        <div className={styles.box}>app</div>
       </p>
     </div>
   );
