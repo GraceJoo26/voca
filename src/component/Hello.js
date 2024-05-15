88d02cc4187eccfb16586a219aacc545b58e96de
graceJoo <gracejoo26@gmail.com>
2024-05-15 17:50:48 +0900

    8강-컴포넌트간 props 사용

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,17 +1,24 @@
 import { useState } from "react";
-export default function Hello() {
+import UserName from "./UserName";
+export default function Hello({ age }) {
   //let name = "Mike";
   const [name, setName] = useState("Mike");
+  const msg = age > 19 ? "성인입니다" : "미성년자입니다";
+  //const [age, setAge] = useState(props.age);
 
   /* function changeName() {
     setName(name === "Mike" ? "Jane" : "Mike");
   } */
   return (
     <div>
-      <h2>{name}</h2>
+      <h2>
+        {name},({age}) : {msg}
+      </h2>
+      <UserName name={name} />
       <button
         onClick={() => {
           setName(name === "Mike" ? "Jane" : "Mike");
+          //etAge(age + 1);
         }}
       >
         Change
